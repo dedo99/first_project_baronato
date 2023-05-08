848134b9741ac3474eea78096c14e74b5c8d5af4
piebaroni <pietroba99@gmail.com>
2023-05-08 13:19:39 +0200

    test third job

--- third_job/map_reduce/reducer.py
+++ third_job/map_reduce/reducer.py
@@ -26,18 +26,20 @@ for line in sys.stdin:
 # utenti -> prodotti in comune
 
 # PER OGNI COPPIA DI UTENTI ABBIAMO LE INTERSEZIONI RILEVANTI TRA I GUSTI
-couple_users2intersection = {}
+common_product2users = {}
+users = list(user2products.keys())
+
 
 # confronti tra tutte le liste degli utenti
-for user1 in user2products:
-    for user2 in user2products: 
-        if user1 == user2:
+for i in range(0, len(users)-1):
+    for j in range(1, len(users)):
+        common_elements = intersection(user2products[users[i]], user2products[users[j]])
+        if len(common_elements) < 3:
             continue
-        intersect = intersection(user2products[user1], user2products[user2])
-        if len(intersect) < 3:
-            continue
-        if (user1, user2) not in couple_users2intersection or (user2, user1) not in couple_users2intersection :
-            couple_users2intersection[(user1, user2)] = intersection
-
-for user1, user2 in couple_users2intersection:
-    print('(%s, \t%s)\t%s' % (user1, user2, '[' + ', '.join(couple_users2intersection[(user1, user2)]) + ']'))
+        fs = frozenset(common_elements)
+        if fs not in common_product2users:
+            common_product2users[fs] = []
+        common_product2users[fs].append([users[i], users[j]])
+    
+for lista in common_product2users:
+    print("%s\t%s" % (lista, common_product2users[lista]))
